6d48d82ea61fd945e492c2dda1a57d29b165e983
Hari KT <kthari85@gmail.com>
2014-11-18 13:09:56 +0530

    Doc block for renderer interface

--- src/Renderer/RendererInterface.php
+++ src/Renderer/RendererInterface.php
@@ -3,6 +3,9 @@ namespace FOA\Responder_Bundle\Renderer;
 
 interface RendererInterface
 {
+    /**
+     * @return string
+     */
     public function render($data, $view, $layout = null);
 
     public function getEngine();
